e1e1003b91ea1419e4b75d805dbabe5fdaed796f
Sean S. LeBlanc <sean.s.leblanc@gmail.com>
2020-06-26 17:18:59 -0400

    expand type

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,16 @@
 import { PostProcessorModule } from "i18next";
 
-const intervalPlural: PostProcessorModule;
+interface IntervalPluralOptions {
+	intervalSeparator: string;
+	intervalRegex: RegExp;
+	intervalSuffix: string;
+}
+
+const intervalPlural: PostProcessorModule & {
+	name: 'interval';
+	type: 'postProcessor';
+	options: IntervalPluralOptions;
+	setOptions: (options: Partial<IntervalPluralOptions>) => void;
+};
+
 export default intervalPlural;
